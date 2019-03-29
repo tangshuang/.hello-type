0f2554e45d9744e93916f90bf646590228cd33d9
tisontang(唐霜) <tisontang@tencent.com>
2019-03-29 18:18:32 +0800

    add extend and shrink

--- src/dict.js
+++ src/dict.js
@@ -1,16 +1,10 @@
 import Type from './type'
-import { isObject } from './utils'
+import { isObject, inObject } from './utils'
 import { _ERROR_, xError } from './error'
 
-export function Dict(pattern) {
-  // if pattern is not an object, it treated undefined
-  if (!isObject(pattern)) {
-    pattern = Object
-  }
 
-  const DictType = new Type(pattern)
-  DictType.name = 'Dict'
-  DictType.assert = function(value) {
+const prototypes = {
+  assert: function(value) {
     if (!isObject(value)) {
       throw new _ERROR_('refuse', { value, type: this, action: 'assert' })
     }
@@ -20,7 +14,39 @@ export function Dict(pattern) {
     if (error) {
       throw xError(error, { value, rule, type: this, action: 'assert' })
     }
+  },
+  extend: function(pattern) {
+    const originalPattern = this.patterns[0]
+    const newPattern = Object.assign({}, originalPattern, pattern)
+    const newType = Dict(newPattern)
+    return newType
+  },
+  shrink: function(pattern) {
+    const originalPattern = this.patterns[0]
+    const originalKeys = Object.keys(originalPattern)
+    const newPattern = {}
+    originalKeys.forEach((key) => {
+      if (pattern[key]) {
+        newPattern[key] = originalPattern[key]
+      }
+    })
+    const newType = Dict(newPattern)
+    return newType
+  },
+}
+
+export function Dict(pattern) {
+  // if pattern is not an object, it treated undefined
+  if (!isObject(pattern)) {
+    pattern = Object
   }
+
+  const DictType = new Type(pattern)
+  DictType.name = 'Dict'
+
+  DictType.assert = prototypes.assert.bind(DictType)
+  DictType.extend = prototypes.extend.bind(DictType)
+  DictType.shrink = prototypes.shrink.bind(DictType)
 
   return DictType
 }
